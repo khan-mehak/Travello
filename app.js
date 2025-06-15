deb751bf757d5c8e00ad487d2a4ca73b8052ebde
mehak khan <mk0345786@gmail.com>
2025-06-16 01:38:42 +0530

    initial commit

--- app.js
+++ app.js
@@ -126,6 +126,6 @@ async function main(){
 }
 
 
-app.listen(8080,()=>{
-    console.log("Server is running on port : 8080")
+app.listen(PORT,()=>{
+    console.log(`Server is running on port : ${PORT}`)
 })

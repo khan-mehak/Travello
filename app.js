fb8fb4dfd90cd542f19a7535473c7e3a4f388279
mehak khan <mk0345786@gmail.com>
2025-06-16 01:52:17 +0530

    initial commit

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.set("view engine","ejs")
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride("_method"))
 app.engine('ejs',ejsMate)
-app.use(express.static(path.join(__dirname,"/public")))
+app.use(express.static(path.join(__dirname,"public")))
 app.use(session(sessionOpt))
 app.use(flash())
 
